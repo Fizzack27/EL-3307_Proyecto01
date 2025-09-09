2b8e8f21ec34f48fb8aa3b60699993c64f67a3ab
Fizzack27 <ifloresm2703@gmail.com>
2025-09-09 03:53:09 -0600

    Correcciones en modulo 5
    - Se agrego un led extra y se corrigió el formato de información.

--- src/design/modulo_05.sv
+++ src/design/modulo_05.sv
@@ -1,9 +1,8 @@
 // Enciende los leds de la fpga, no enciende ninguno si hay 2 errores
 module modulo_05 (
     input logic [4:0] w_corregida_b4,
-    output logic [3:0] leds;
+    output logic [4:0] led;
 );
-    logic [3:0] bits_info;
-    assign bits_info = {w_corregida_b4[2], w_corregida_b4[1], w_corregida_b4[0]} // se obtiene solo la informacion de la palabra
-    assign leds = (w_corregida_b4[3] == 1'b1) ? 4'b111 : ~bits_info; // se encienden los leds
+    assign led = (w_corregida_b4[3] == 1'b1) ? 4'b0111 : ~w_corregida_b4; // se encienden los leds
+
 endmodule
